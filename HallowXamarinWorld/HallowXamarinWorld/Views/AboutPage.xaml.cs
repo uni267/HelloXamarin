aba408b2fac56b7837a35551b752394be71b31ef
GR-OPS-0028-PC\n267 <m-uenoyama@cyber-l.co.jp>
2017-04-14 13:32:46 +0900

    プロジェクト ファイルを追加します。

---
+++ HallowXamarinWorld/HallowXamarinWorld/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace HallowXamarinWorld.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
